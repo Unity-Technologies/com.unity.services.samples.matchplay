1914fbc72ac961c7a14ee2f57294b80f3b415d94
jacob.lorentzen@unity3d.com <>
2022-05-13 15:41:40 -0700

    Moved assemblies and wrote the Server Setup tests.

---
+++ Assets/Scripts/Tests/Runtime/ServerTests.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using Matchplay.Server;
+using Matchplay.Shared;
+using NUnit.Framework;
+using Unity.Netcode;
+using Unity.Services.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Matchplay.Tests
+{
+	public class ServerTests
+	{
+		NetworkManager m_TestManager;
+
+		const string k_LocalIP = "127.0.0.1";
+		const int k_DefaultPort = 7777;
+		const int k_DefaultQPort = 7787;
+
+		[OneTimeSetUp]
+		[RequiresPlayMode]
+		public void OneTimeSetup()
+		{
+			ApplicationData.IsServerTesting = true;
+			m_TestManager = TestResources.TestNetworkManager();
+			UnityServices.InitializeAsync(); // Needs to be intitialized in a unity thread.
+		}
+
+		[TearDown]
+		[RequiresPlayMode]
+		public void TearDown()
+		{
+			if (m_TestManager.IsListening)
+			{
+				m_TestManager.Shutdown();
+			}
+		}
+
+		[UnityTest]
+		[RequiresPlayMode]
+		public IEnumerator CreateLocalServer_Staring_Lab_Casual()
+		{
+			var startingGameInfo = new GameInfo
+			{
+				gameMode = GameMode.Staring,
+				map = Map.Lab,
+				gameQueue = GameQueue.Casual
+			};
+
+			var createServerTask = CreateServerAsync(k_LocalIP, k_DefaultPort, k_DefaultQPort,
+				startingGameInfo);
+
+			yield return new WaitUntil(() => createServerTask.IsCompleted);
+			var createdServer = createServerTask.Result;
+			Assert.AreEqual(SceneManager.GetActiveScene(), SceneManager.GetSceneByName(startingGameInfo.ToScene));
+			Assert.NotNull(createdServer.ServerData);
+			Assert.AreEqual(startingGameInfo.gameMode, createdServer.ServerData.gameMode.Value);
+			Assert.AreEqual(startingGameInfo.map, createdServer.ServerData.map.Value);
+			Assert.AreEqual(startingGameInfo.gameQueue, createdServer.ServerData.gameQueue.Value);
+			Assert.IsFalse(createdServer.StartedServices);
+		}
+
+		[UnityTest]
+		[RequiresPlayMode]
+		public IEnumerator CreateLocalServer_Meditating_Space_Competitive()
+		{
+			var startingGameInfo = new GameInfo
+			{
+				gameMode = GameMode.Meditating,
+				map = Map.Space,
+				gameQueue = GameQueue.Competetive
+			};
+			var createServerTask = CreateServerAsync(k_LocalIP, k_DefaultPort, k_DefaultQPort,
+				startingGameInfo);
+
+			yield return new WaitUntil(() => createServerTask.IsCompleted);
+			var createdServer = createServerTask.Result;
+			Assert.AreEqual(SceneManager.GetActiveScene(), SceneManager.GetSceneByName(startingGameInfo.ToScene));
+			Assert.NotNull(createdServer.ServerData);
+			Assert.AreEqual(startingGameInfo.gameMode, createdServer.ServerData.gameMode.Value);
+			Assert.AreEqual(startingGameInfo.map, createdServer.ServerData.map.Value);
+			Assert.AreEqual(startingGameInfo.gameQueue, createdServer.ServerData.gameQueue.Value);
+			Assert.IsFalse(createdServer.StartedServices);
+		}
+
+
+		async Task<ServerGameManager> CreateServerAsync(string ip, int port, int qport, GameInfo gameInfo)
+		{
+			var serverGameManager = new ServerGameManager(ip, port, qport, NetworkManager.Singleton);
+			await serverGameManager.StartGameServerAsync(gameInfo);
+			return serverGameManager;
+		}
+
+
+	}
+}
