8330ed80f846275b0efea967962a96e6cf4a8407
u-christianburberry <christian.burberry@unity3d.com>
2022-08-09 11:06:31 +0100

    Updated MM/MP SDK dependency versions.
    - Removed local packages.
    - Updated MM to 1.0.0-pre.12
    - Updated MP to 1.0.0-pre.5
    - Replaced local package of ParrelSync to git target in manifest.json.

--- Packages/com.unity.services.multiplay@1.0.0-pre.2/Runtime/EmbedWire/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.CompilerServices;
-
-using UnityEngine.Scripting;
-
-// prevent Il2CPP code stripping
-[assembly: AlwaysLinkAssembly]
-
-[assembly: InternalsVisibleTo("Unity.Services.Wire.Tests")]
-[assembly: InternalsVisibleTo("Unity.Services.Wire.EditorTests")]
